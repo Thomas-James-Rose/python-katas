85a05f0ca8afc7a4f61c622128cb6abecda1b43d
Tom Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2021-02-19 15:21:56 +0000

    Refactor and continue to implement Knight's Attack kata

--- knights-attack/knights_attack.py
+++ knights-attack/knights_attack.py
@@ -6,6 +6,19 @@ def print_chess_board(board):
       sys.stdout.write(y)
     sys.stdout.write("\n")
 
+
+def calculate_next_move(distance_to_end):
+  if(distance_to_end[0] >= 2 and distance_to_end[1] >= 1):
+    return (2, 1)
+  elif(distance_to_end[0] <= -2 and distance_to_end[1] >= 1):
+    return (-2, 1)
+  elif(distance_to_end[1]) == 0:
+    return (1, 2)
+  elif(distance_to_end[0] >= 1 and distance_to_end[1] <= -2):
+    return (1, -2)
+  else:
+    return (0, 0)
+
 def move_knight(current_position, move): 
   return tuple(sum(t) for t in zip(current_position, move))
 
@@ -13,22 +26,22 @@ def knights_attack(start, end, obstacles):
   knight_pos = start
   distance_to_end = tuple(t[1] - t[0] for t in zip(knight_pos, end))
 
-  chess_board = [["[ ]" for x in range(8)] for y in range(8)]
+  chess_board = [["[ ]" for y in range(8)] for x in range(8)]
   chess_board[knight_pos[0]][knight_pos[1]] = "[K]"
   chess_board[end[0]][end[1]] = "[O]"
   print_chess_board(chess_board)
 
   move_num = 0
-  while distance_to_end != (0, 0):
+  while distance_to_end != (0, 0) and move_num < 20:
     move_num += 1
     print(f"\n Move {move_num}")
     chess_board[knight_pos[0]][knight_pos[1]] = "[ ]"
-    if(distance_to_end[0]) >= 2:
-      knight_pos = move_knight(knight_pos, (2, 1))
+    knight_pos = move_knight(knight_pos, calculate_next_move(distance_to_end))
     chess_board[knight_pos[0]][knight_pos[1]] = "[K]"
     distance_to_end = tuple(t[1] - t[0] for t in zip(knight_pos, end))
+    print(distance_to_end)
     print_chess_board(chess_board)
 
   print("\n Done!")
   
-knights_attack((1,1), (3,2), [])
+knights_attack((1,1), (5,2), [])
